4217c31355bcda2de9b6e7e01c00e8a59943d62c
Adrian Eguez <eadepto@hotmail.com>
2023-11-15 08:26:44 -0500

    hasta funciuon

--- 01 - KOTLIN/proyecto/src/main/kotlin/Main.kt
+++ 01 - KOTLIN/proyecto/src/main/kotlin/Main.kt
@@ -24,4 +24,43 @@ fun main(){
     val mayorEdad: Boolean = true
     // Clases Java
     val fechaNacimiento: Date = Date()
+
+    // SWITCH
+    val estadoCivilWhen = "C"
+    when (estadoCivilWhen) {
+        ("C") -> {
+            println("Casado")
+        }
+        "S" -> {
+            println("Soltero")
+        }
+        else -> {
+            println("No sabemos")
+        }
+    }
+    val esSoltero = (estadoCivilWhen == "S")
+    val coqueteo = if (esSoltero) "Si" else "No"
+
+}
+
+// void -> Unit
+fun imprimirNombre(nombre: String): Unit{
+    // "Nombre : " + nombre
+    println("Nombre : ${nombre}") // template strings
+}
+
+
+fun calcularSueldo(
+    sueldo: Double, // Requerido
+    tasa: Double = 12.00, // Opcional (defecto)
+    bonoEspecial: Double? = null, // Opcion null -> nullable
+): Double{
+    // Int -> Int? (nullable)
+    // String -> String? (nullable)
+    // Date -> Date? (nullable)
+    if(bonoEspecial == null){
+        return sueldo * (100/tasa)
+    }else{
+        return sueldo * (100/tasa ) + bonoEspecial
+    }
 }
