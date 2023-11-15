8750d87c1c1393dbea8c2c3bc86a63ec723ecab7
Adrian Eguez <eadepto@hotmail.com>
2023-11-15 07:50:31 -0500

    hasta variables

--- 01 - KOTLIN/proyecto/src/main/kotlin/Main.kt
+++ 01 - KOTLIN/proyecto/src/main/kotlin/Main.kt
@@ -1,3 +1,27 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.*
+
+fun main(){
+    println("Hola mundo")
+    // INMUTABLES (NO se reasignan "=")
+    val inmutable: String = "Adrian";
+    // inmutable = "Vicente";
+
+    // Mutables (Re asignar)
+    var mutable: String = "Vicente";
+    mutable = "Adrian";
+
+    //  val > var
+    // Duck Typing
+    var ejemploVariable = " Adrian Eguez "
+    val edadEjemplo: Int = 12
+    ejemploVariable.trim()
+    // ejemploVariable = edadEjemplo;
+
+    // Variable primitiva
+    val nombreProfesor: String = "Adrian Eguez"
+    val sueldo: Double = 1.2
+    val estadoCivil: Char = 'C'
+    val mayorEdad: Boolean = true
+    // Clases Java
+    val fechaNacimiento: Date = Date()
 }
