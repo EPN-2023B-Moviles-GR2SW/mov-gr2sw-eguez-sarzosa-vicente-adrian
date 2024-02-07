e5a7f807697c22b79470cee472d257fb01d195e5
Adrian Eguez <eadepto@hotmail.com>
2024-02-07 08:20:02 -0500

    final

--- 02-Android/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
+++ 02-Android/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
@@ -138,6 +138,10 @@ class MainActivity : AppCompatActivity() {
         intentExplicito.putExtra("apellido", "Eguez")
         intentExplicito.putExtra("edad", 34)
 
+        intentExplicito.putExtra("entrenador",
+            BEntrenador(1,"Nombre","Descripcion")
+            )
+
         callbackContenidoIntentExplicito.launch(intentExplicito)
     }
     fun irActividad(
